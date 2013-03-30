e0b5e8a5814ab8ce3362266547c52a6cd130b5d3
Ben Iofel <iofelben@gmail.com>
2013-03-30 17:52:43 -0400

    not using frame.gestures anymore

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,6 +6,8 @@
 
 import com.leapmotion.leap.*;
 import java.io.*;
+import java.util.ArrayList;
+
 import javax.sound.midi.MidiUnavailableException;
 
 /*
@@ -20,13 +22,14 @@ public class MainClass {
 		con.addListener(lis);
 
 		lis.audio = new Audio();
-
+		
 		System.out.println("Press any key to quit...");
 		try {
 			System.in.read();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 		System.out.println("quitting");
 		con.removeListener(lis);
 	}
@@ -37,19 +40,22 @@ class Lis extends Listener{
 	LeapInstrument ins;
 	Finger finger;
 	Hand hand;
-	//	float maxX=0,maxY=0,maxZ=0,minX=0,minY=0,minZ=0;
-	float curx=0,cury=0,curz=0;
+	Frame frame;
+	int calibratedY;
+	Thread t;
+	ArrayList<Float> calibratingValues = new ArrayList<Float>();
+	boolean calibrating = false, detectingTaps = false;
+
+	float currentFingerX=0,currentFingerY=0,currentFingerZ=0;
 	public Audio audio;
 
 	//	instrument X axis mapping vars
 	int xAxisMin = -200;
 	int xAxisMax = 200;
 	int numTapAreas = 7;
-	int tapAreaSize = (xAxisMax-xAxisMin)/numTapAreas;
-//	int[] tapAreas = new int[numTapAreas];
-	
-	
-	boolean drums = true; //false = piano
+	int tapAreaSize = (xAxisMax-xAxisMin)/numTapAreas;	
+
+	boolean drums = false; //false = piano
 
 	//overriding default listener methods
 	public void onInit(Controller controller){
@@ -59,100 +65,115 @@ class Lis extends Listener{
 	public void onConnect(Controller controller) {
 		System.out.println("LEAP Connected");
 		controller.enableGesture(Gesture.Type.TYPE_KEY_TAP); //enable only key tap gesture
-//		for(int i=0;i<numTapAreas;i++){
-//			tapAreas[i] = xAxisMin+(i*tapAreaSize);
-//		}
+		//calibrate Y value 
+		t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("calibrating in\n3...");
+				try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+				System.out.println("2...");
+				try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+				System.out.println("1...");
+				calibrating = true;
+				try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+				//do after 2000 milliseconds
+				int sum = 0, size = calibratingValues.size();
+				for(int i=0;i<size;i++) sum += calibratingValues.get(i);	//add the y values
+				if(size < 100){							//too few y values
+					System.out.println("not enough y values. please try again");
+					System.exit(0);
+				} else {
+					calibratedY = sum/size;				//avg of y values
+					System.out.println("calibrated Y: " + calibratedY);
+					if(calibratedY < 100){
+						System.out.println("calibrated Y value is too low. please try again");
+						System.exit(0);
+					}
+					calibrating = false;
+					detectingTaps = true;
+				}
+			}
+		});
+		t.start();
+		System.out.println("calibrated y: " + calibratedY);
 	}
 
 	public void onFrame(Controller controller){
-		Frame frame = controller.frame();
+		frame = controller.frame();
 		GestureList ges = frame.gestures();
-		Gesture curGes;
-		//		FingerList fingerlist = frame.fingers();
-
-		//		System.out.print("Frame id: " + frame.id() + " timestamp: " + frame.timestamp());
-
-		if(!frame.hands().empty()){ //some hands detected
-			hand = frame.hands().get(0); //get 1st hand
-		}
-		if(!frame.fingers().empty()){ //some fingers detected
-			finger = frame.fingers().get(0); //get 1st finger
-		}
-
-		if(!frame.fingers().empty()){
-			curx = finger.tipPosition().getX();
-			cury = finger.tipPosition().getY();
-			curz = finger.tipPosition().getZ();
-			if(cury<250){	//y limit
-				if(ges.count() > 1) {		//only 1 tap at a time
-					System.out.println("more than 1 gesture detected");
-				} else if(ges.count()==1){
-					System.out.println("tap detected");
-					curGes = ges.get(0);
-					float gesFinPosX = curGes.pointables().get(0).tipPosition().getX();
-					//					System.out.println(curGes.type() + " at x: " + gesFinPos.getX() + " y: " + gesFinPos.getY() + " z: " +gesFinPos.getZ());
-//					for(int i=0;i<numTapAreas;i++) if(gesFinPosX>tapAreas[i] && gesFinPosX<=tapAreas[i]+tapAreaSize){
-					int i = ((int)gesFinPosX + xAxisMax) / tapAreaSize;
-					if(!drums){
-						try{
-							int note = 0;
-							switch(i) {
-							case 0: note = 0; break;	//C
-							case 1: note = 2; break;	//D
-							case 2: note = 4; break;	//E
-							case 3: note = 5; break;	//F
-							case 4: note = 7; break;	//G
-							case 5: note = 9; break;	//A
-							case 6: note = 11; break;	//B
-							}
-							ins = new Piano(1);
-							ins.playNote(note, octave);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					} else {
-						String drum = "";
+		finger = frame.fingers().get(0);
+		
+		if(calibrating){
+			if(frame.fingers().count() != 1) System.out.println("wrong number of fingers. please only show 1");
+			else calibratingValues.add(finger.tipPosition().getY());
+		} else if (detectingTaps && !frame.fingers().empty()){
+			// MAIN LOOP
+			finger = frame.fingers().get(0);
+			currentFingerX = finger.tipPosition().getX();
+			currentFingerY = finger.tipPosition().getY();
+			currentFingerZ = finger.tipPosition().getZ();
+			if(calibratedY-finger.tipPosition().getY()>30){ // ON PRESS
+				System.out.println("tap detected");
+				float keyPressXPosition = finger.tipPosition().getX();
+				int i = ((int)keyPressXPosition + xAxisMax) / tapAreaSize;
+				if(!drums){
+					try{
+						int note = 0;
 						switch(i) {
-						case 0:
-							drum = "Kick";
-							break;
-						case 1:
-							drum = "Snare";
-							break;
-						case 2:
-							drum = "HiHat";
-							break;
-						case 3:
-							drum = "Crash";
-							break;
-						case 4:
-							drum = "Floor Tom";
-							break;
-						case 5:
-							drum = "Low Tom";
-							break;
-						case 6:
-							drum = "High Tom";
-							break;
+						case 0: note = 0; break;	//C
+						case 1: note = 2; break;	//D
+						case 2: note = 4; break;	//E
+						case 3: note = 5; break;	//F
+						case 4: note = 7; break;	//G
+						case 5: note = 9; break;	//A
+						case 6: note = 11; break;	//B
 						}
-						try {
-							ins = new DrumKit(1);
-							ins.playDrum(drum);
-							System.out.println("Playing " + drum);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
+						ins = new Piano(1);
+						ins.playNote(note, octave);
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
-					//					}
+				} else {
+					String drum = "";
+					switch(i) {
+					case 0:
+						drum = "Kick";
+						break;
+					case 1:
+						drum = "Snare";
+						break;
+					case 2:
+						drum = "HiHat";
+						break;
+					case 3:
+						drum = "Crash";
+						break;
+					case 4:
+						drum = "Floor Tom";
+						break;
+					case 5:
+						drum = "Low Tom";
+						break;
+					case 6:
+						drum = "High Tom";
+						break;
+					}
+					try {
+						ins = new DrumKit(1);
+						ins.playDrum(drum);
+						System.out.println("Playing " + drum);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}
 	}
-	
+
 	private void changeOctave(int change) {
 		octave += change;
 	}
-	
+
 	public void onDisconnect(Controller controller) {
 		System.out.println("LEAP Disconnected");
 	}
